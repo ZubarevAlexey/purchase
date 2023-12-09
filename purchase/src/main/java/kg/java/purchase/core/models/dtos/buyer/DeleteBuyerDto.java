4c187252fef414454ca6043226f85cfcecb5de41
User <stparchkg@gmail.com>
2023-12-09 21:46:40 +0600

    add dtos for BuyerEntity add BuyerMapper

---
+++ purchase/src/main/java/kg/java/purchase/core/models/dtos/buyer/DeleteBuyerDto.java
@@ -0,0 +1,12 @@
+package kg.java.purchase.core.models.dtos.buyer;
+
+import kg.java.purchase.core.models.dtos.base.BaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeleteBuyerDto extends BaseDto {
+
+
+}
