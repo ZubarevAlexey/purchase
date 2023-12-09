41358497e4e1747e902c3e19ead4eface1fd262b
User <stparchkg@gmail.com>
2023-12-09 22:25:54 +0600

    add method FindByBuyerName in PurchaseService

--- purchase/src/main/java/kg/java/purchase/core/contracts/services/PurchaseService.java
+++ purchase/src/main/java/kg/java/purchase/core/contracts/services/PurchaseService.java
@@ -5,10 +5,13 @@ import kg.java.purchase.core.exceptions.EntityNotFoundException;
 import kg.java.purchase.core.models.dtos.purchase.*;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+
 public interface PurchaseService {
     PurchaseDto add(CreatePurchaseDto model) throws EntityNotFoundException;
     PurchaseDto update(UpdatePurchaseDto model) throws EntityDuplicateException, EntityNotFoundException;
     HttpStatus delete(DeletePurchaseDto model) throws EntityNotFoundException;
     PurchaseDto findById(FindByIdPurchaseDto model) throws EntityNotFoundException;
+    List<PurchaseDto> findByBuyerName(FindByBuyerNameDto model);
 
 }
