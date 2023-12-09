2752a083af7d85455ab44edb18736ccaa6584fb9
User <stparchkg@gmail.com>
2023-12-09 21:41:01 +0600

    add BuyerEntity add BuyerRepository

---
+++ purchase/src/main/java/kg/java/purchase/data/BuyerRepository.java
@@ -0,0 +1,19 @@
+package kg.java.purchase.data;
+
+
+import kg.java.purchase.core.models.entities.BuyerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BuyerRepository extends JpaRepository<BuyerEntity, Long> {
+    Optional<BuyerEntity> findByName(String name);
+// поиск покупателей по вхождению имени
+    List<BuyerEntity> findBuyerEntitiesByNameContainsIgnoreCase(String text);
+
+
+
+}
